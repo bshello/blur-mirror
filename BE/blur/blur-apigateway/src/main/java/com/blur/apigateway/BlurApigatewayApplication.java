5b20b58134ff119b32d5706fc8f615cc4e8c4d02
bshello25 <bshello25@gmail.com>
2023-02-09 15:43:02 +0900

    :art: cors 해결
    cors 해결
    
    
    Former-commit-id: 6488c91b2db18343aebbcc57cbaf535e7645856c

--- BE/blur/blur-apigateway/src/main/java/com/blur/apigateway/BlurApigatewayApplication.java
+++ BE/blur/blur-apigateway/src/main/java/com/blur/apigateway/BlurApigatewayApplication.java
@@ -2,6 +2,9 @@ package com.blur.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class BlurApigatewayApplication {
