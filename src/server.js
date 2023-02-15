2171f534a928a11a0aa12da6e5adc2c848f2cd0b
김창겸 <kyum8562@naver.com>
2023-02-15 22:02:31 +0900

    :bug: test
    test

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ const httpServer = http.createServer(app);
 // // http 서버 위에 ws(webSocket) 서버를 만듦
 const wsServer = new Server(httpServer, {
   cors: {
-    origin: "[https://admin.socket.io, https://i8b307.p.ssafy.io/]",
+    origin: "[https://admin.socket.io, https://i8b307.p.ssafy.io]",
     credentials: true,
   },
 });
