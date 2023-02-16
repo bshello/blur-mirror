f4eb919e81c2f7c22dd3aa3566ea5c6b454b57b6
songdaehyun <goflwla92@naver.com>
2023-02-16 21:39:58 +0900

    :art: push
    push

--- FE/src/pages/NotFound/NotFound.jsx
+++ FE/src/pages/NotFound/NotFound.jsx
@@ -1,10 +1,8 @@
 import "./NotFound.css";
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 function NotFound() {
-  const navigate = useNavigate();
   return (
     <div>
       <Link to="/home">
